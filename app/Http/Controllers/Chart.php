16531320dc107178a647ceac6a0963a76ce63dcc
Pratik Bassi <pratik.bassi@mail.utoronto.ca>
2020-06-17 08:34:09 +0000

    removed auth requirement to see graphs

--- app/Http/Controllers/Chart.php
+++ app/Http/Controllers/Chart.php
@@ -17,7 +17,7 @@ class Chart extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware('auth', ['except' => ['show']]);
     }
 
     /**
